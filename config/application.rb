45af42e3248840c808431c0dcb96efc4ec13a57e
Tim Passmore <tnpassmore@westnet.com.au>
2013-09-27 18:23:21 +1000

    Add layout partials.

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+# http://stackoverflow.com/questions/14980871/importing-bootstrap-with-rails-errror
+require "bootstrap-sass" 
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +26,6 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
