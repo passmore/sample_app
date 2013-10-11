ff39e7d877e7d758665fd98baef6786839f0eaf0
Tim Passmore <tnpassmore@westnet.com.au>
2013-10-11 16:38:05 +1000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Use strong parameters!
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
